9c8c62f5f0b13149e29201745187dc418c72bb8e
Craig Atkinson <catkinson@bloomhealthco.com>
2013-10-15 16:40:49 -0500

    Consume output from grails test app process to avoid tests stalling when process output fills up buffer

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -73,9 +73,13 @@ environments {
 log4j = {
     // Example of changing the log pattern for the default console appender:
     //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    appenders {
+        file name:'file', file: 'target/grails.log'
+    }
+
+    root {
+        info 'file'
+    }
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP
