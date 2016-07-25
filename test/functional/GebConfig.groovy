84eee3f801276cf35f445c0a096aa04dc0f9723d
catkinson <catkinson@bloomhealthco.com>
2016-07-25 12:01:07 -0500

    Updating to Selenium 2.53.1

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,4 +1,14 @@
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.firefox.FirefoxDriver
+
 waiting {
   timeout = 15
   retryInterval = 0.5
+}
+
+driver = {
+  def theDriver = new FirefoxDriver()
+  theDriver.manage().window().setSize(new Dimension(600, 800))
+
+  return theDriver
 }
